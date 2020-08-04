04d2bc77c413342f2135f126127d06c9cccc3691
ramonapara <62647194+ramonapara@users.noreply.github.com>
2020-08-04 11:03:17 +0300

    Create example.c

---
+++ #7/#solutie1/example.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "example.h"
+#include <dlfcn.h>
+
+//gcc prob7.c example.so
+//./a.out
+//
+//
+// gcc -o myprogram prob7-project2.c -ldl -rdynamic
+// gcc -fPIC -shared -o example.so prob7-project2.c
+int  *allocInLib()
+{
+	int* ptr=(int *)malloc(sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("Ati alocat gresit. \n");
+		exit(0);
+	}
+	else
+	{
+		printf("Ati alocat memorie corect! .\n");
+		printf("Val: %p \n",&ptr);
+	}
+	return ptr;
+}
+
+void freeInLib(int *x)
+{
+	free(x);
+	printf("Ati dealocat memoria pentru numar. \n");
+}
