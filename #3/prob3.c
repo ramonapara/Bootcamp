98d2ad825bc9a3546af38e60bf88c37a847cd2e3
ramonapara <62647194+ramonapara@users.noreply.github.com>
2020-08-04 10:58:37 +0300

    Create prob3.c

---
+++ #3/prob3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void progressCallback( int index, int dataSize)
+{
+	int rez=dataSize-index;
+	float  percentage=(float)index/dataSize*100.0;
+	printf("Procentaj curent: ");
+	printf("%lf",percentage);
+	printf("\n");
+}
+
+void handleData(void(*f)(int,int),char *data, int dataSize)
+{
+      int i;
+      int index=0;
+      for(i=0;i<dataSize;i++)
+      {
+	      printf("Date: ");
+	      printf("%c",data[i]);
+	      printf("\n");
+	      index++;
+	      (*f)(index,dataSize);
+      }
+
+}
+
+int main()
+{
+	int dataSize;
+	printf("dataSize = ");
+	scanf("%d",&dataSize);
+	printf("%d",dataSize);
+
+	char *data;
+	printf("data = ");
+	scanf("%s",data);
+	printf("%s",data);
+	//idata=(char *)malloc(dataSize*sizeof(char));
+	handleData(progressCallback,data,dataSize);
+
+	return 0;
+}
+
+
